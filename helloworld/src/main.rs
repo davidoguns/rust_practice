ae1fa222a4d53abb4e43e3090db838dd919f5827
David Oguns <davidoguns@gmail.com>
2022-01-09 19:42:25 -0800

    Messed around in hello world. Small changes in quickreplace.

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -1,20 +1,56 @@
 use std::io::Write;
 use std::io;
-use rand::Rng;
+
+struct Book {
+    title: String,
+    isbn: String,
+    avg_review: f32,
+    num_ratings: u32,
+}
 
 fn main() {
     println!("Hello, world!");
 
-    let num = rand::thread_rng().gen_range(1, 1000);
+    let books = vec![
+        Book { title: "Harry Potter".to_string(),
+               isbn: "1234-56-7890".to_string(),
+               avg_review: 9.2,
+               num_ratings: 15687,
+            },
+        Book { title: "The Lord of the Rings".to_string(),
+               isbn: "8234-31-4429".to_string(),
+               avg_review: 8.2,
+               num_ratings: 35413,
+            }
+    ];
 
-    println!("Number is: {}", num);
 
-    let mut guess =  String::new();
+    for book in books {
+        println!("Book title: {}; Average Review: {:.2}",
+                 book.title, book.avg_review);
+        println!("Num Ratings: {}; ISBN: {}",
+                 book.num_ratings, book.isbn);
+    }
 
-    print!("Please enter your guess: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut guess)
-	.expect("Couldn't read guess from stdin");
+    loop {
+        print!("Please enter your guess: ");
+        io::stdout().flush().unwrap();
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Couldn't read guess from stdin");
 
-    println!("Your guess is: {}", guess);
+        let x = guess.trim_end().parse::<i32>();
+        match x {
+            Ok(num) =>
+                println!("Your guess is a valid number {}", num),
+            Err(_) => {
+                if guess.trim_end().to_lowercase() == "quit" ||
+                    guess.trim_end().to_lowercase() == "q" {
+                    break;
+                } else {
+                    println!("Your guess is not a valid number");
+                }
+            }
+        }
+    }
 }
--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,7 +1,7 @@
-use text_colorizer::*;
+use regex::Regex;
 use std::env;
 use std::fs;
-use regex::Regex;
+use text_colorizer::*;
 
 #[derive(Debug)]
 struct Arguments {
@@ -12,8 +12,10 @@ struct Arguments {
 }
 
 fn print_usage() {
-    eprintln!("{} - change occurrences of one string into another",
-              "quickreplace".green().bold());
+    eprintln!(
+        "{} - change occurrences of one string into another",
+        "quickreplace".green().bold()
+    );
     eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
 }
 
@@ -22,16 +24,20 @@ fn parse_args() -> Arguments {
 
     if args.len() != 4 {
         print_usage();
-        eprintln!("{} wrong number of arguments: expected 4, got {}.",
-                  "Error:".red().italic(), args.len());
+        eprintln!(
+            "{} wrong number of arguments: expected 4, got {}.",
+            "Error:".red().italic(),
+            args.len()
+        );
 
         std::process::exit(1);
     }
 
-    Arguments { target: args[0].clone(),
-                replacement: args[1].clone(),
-                filename: args[2].clone(),
-                output: args[3].clone()
+    Arguments {
+        target: args[0].clone(),
+        replacement: args[1].clone(),
+        filename: args[2].clone(),
+        output: args[3].clone(),
     }
 }
 
@@ -46,8 +52,12 @@ fn main() {
     let data = match fs::read_to_string(&args.filename) {
         Ok(v) => v,
         Err(e) => {
-            eprintln!("{} failed to read from file '{}': {:?}",
-                "Error:".red().bold(), args.filename, e);
+            eprintln!(
+                "{} failed to read from file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
             std::process::exit(1);
         }
     };
@@ -55,19 +65,21 @@ fn main() {
     let replaced_data = match replace(&args.target, &args.replacement, &data) {
         Ok(v) => v,
         Err(e) => {
-            eprintln!("{} failed to replace text: {:?}",
-                "Error:".red().bold(), e);
+            eprintln!("{} failed to replace text: {:?}", "Error:".red().bold(), e);
             std::process::exit(1);
         }
     };
 
     match fs::write(&args.output, &replaced_data) {
-        Ok(_) => {},
+        Ok(_) => {}
         Err(e) => {
-            eprintln!("{} failed to write to file '{}': {:?}",
-                "Error:".red().bold(), args.output, e);
+            eprintln!(
+                "{} failed to write to file '{}': {:?}",
+                "Error:".red().bold(),
+                args.output,
+                e
+            );
             std::process::exit(1);
         }
     };
 }
-
