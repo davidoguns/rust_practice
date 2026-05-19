723289ac441a15ffcb618829c85ab74909d08c82
David Oguns <davidoguns@gmail.com>
2026-05-18 18:20:10 -0700

    upd: wip

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,16 +1,28 @@
-use std::env;
 use minigrep::search::SearchConfig;
+use std::{env, error::Error};
 
 fn parse_config(args: &[String]) -> Option<SearchConfig> {
     match args.len() {
-        2 => Some(SearchConfig::new(args[2].clone(), args[1].clone())),
+        2 => Some(SearchConfig::new(&args[2], &args[1])),
         _ => None,
     }
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let cfg = parse_config(&args);
 
-    println!("Hello, world!");
+    match cfg {
+        Some(cfg) => {
+            println!(
+                "Searching file [{}] for expression/term: [{}]",
+                cfg.file_path(),
+                cfg.search_term()
+            );
+            Ok(())
+        }
+        None => Err(Box::<std::io::Error>::new(std::io::Error::other(
+            "Failed to parse CLI args",
+        ))),
+    }
 }
--- minigrep/src/search.rs
+++ minigrep/src/search.rs
@@ -4,12 +4,44 @@ pub struct SearchConfig {
 }
 
 impl SearchConfig {
-    pub fn new(file_path: String, search_term: String) -> SearchConfig {
+    pub fn new(file_path: &String, search_term: &String) -> SearchConfig {
         SearchConfig {
             file_path: String::from(file_path),
             search_term: String::from(search_term),
         }
     }
+
+    pub fn file_path(&self) -> &String {
+        &self.file_path
+    }
+
+    pub fn search_term(&self) -> &String {
+        &self.search_term
+    }
 }
 
 pub struct SearchLineResult {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test_search_config() {
+        let fp = String::from("foofile");
+        let st = String::from("barterm");
+        let search_config = SearchConfig::new(&fp, &st);
+        assert!(
+            fp.cmp(search_config.file_path()) == std::cmp::Ordering::Equal,
+            "Expected filepath to be [{}], found [{}]",
+            fp,
+            search_config.file_path()
+        );
+        assert!(
+            st.cmp(search_config.search_term()) == std::cmp::Ordering::Equal,
+            "Expected search term to be [{}], found [{}]",
+            st,
+            search_config.search_term()
+        );
+    }
+}
