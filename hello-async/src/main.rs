87d2f9a26789cbb0644ecdf0c0e7ea70804a3e95
David Oguns <davidoguns@gmail.com>
2026-06-19 17:49:08 -0700

    wip: lose changes. Nothing significant.

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -1,6 +1,6 @@
 use trpl::Html;
 
-async fn get_page_title(url: &str) -> Option<String> {
+async fn _get_page_title(url: &str) -> Option<String> {
     let response = trpl::get(url).await;
     let response_text = response.text().await;
     Html::parse(&response_text)
@@ -18,13 +18,22 @@ fn page_title(url: &str) -> impl Future<Output = Option<String>> {
 }
 
 fn main() {
-    println!("Hello, world!");
-    let url = "blahblah";
-    let async_some_work = async move {
-        let text = trpl::get(url).await.text().await;
-        Html::parse(&text)
-            .select_first("title")
-            .map(|title| title.inner_html())
-    };
+    let args : Vec<String> = std::env::args().collect();
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    rt.block_on(async {
+        let url = &args[1];
+        match page_title(url).await {
+            Some(title) => {
+                println!("Page title for {url} was {title}");
+            },
+            None => {
+                println!("No page title for URL: {url}");
+            }
+        }
+    });
 }
 
